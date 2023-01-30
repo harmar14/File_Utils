f221d20e88c06ad57ed6c6f2c05039b6c09701a9
Iuliia Belokon <75740030+harmar14@users.noreply.github.com>
2023-01-30 23:39:18 +0300

    -

--- remove_repeats.py
+++ remove_repeats.py
@@ -1,11 +1,11 @@
 import os
 
 def checkFiles(mainDirName, folderName):
-    print(mainDirName)
+    # print(mainDirName)
     dirName = mainDirName + '/' + folderName
     for file in os.listdir(dirName):
         tmpDir = dirName + '/' + file
-        print(tmpDir, os.path.isdir(tmpDir))
+        # print(tmpDir, os.path.isdir(tmpDir))
         if (os.path.isdir(tmpDir) == True):
             checkFiles(dirName, file)
         else:
@@ -16,12 +16,12 @@ def checkFiles(mainDirName, folderName):
 directory = str(input("Введите путь к расположению двух папок, которые будут сравниваться (он должен быть одинаковым): "))
 folder1, folder2 = [str(i) for i in input("Введите имена папок через пробел: ").split()]
 
-#пример:
-#directory = "C:/Users/julia/Desktop"
-#folder1 = "dir1"
-#folder2 = "dir2"
-#тогда пути к папкам: C:/Users/julia/Desktop/dir1 и C:/Users/julia/Desktop/dir2
-#в результате выполнения программы все файлы папки dir1, имеющие то же имя и уровень вложенности в папке dir2, будут стерты
+# пример:
+# directory = "C:/Users/julia/Desktop"
+# folder1 = "dir1"
+# folder2 = "dir2"
+# тогда пути к папкам: C:/Users/julia/Desktop/dir1 и C:/Users/julia/Desktop/dir2
+# в результате выполнения программы все файлы папки dir1, имеющие то же имя и уровень вложенности в папке dir2, будут стерты
 
 checkFiles(directory, folder2)
 print("done")
